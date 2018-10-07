9891a0d5ec7d36f7c9681994dd7fd1cfd100fd27
Matos <s143485@student.dtu.dk>
2018-10-07 15:47:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/matos/project1/Product.java
@@ -0,0 +1,13 @@
+package com.example.matos.project1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+}

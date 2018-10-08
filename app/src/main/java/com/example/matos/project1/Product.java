630b88e60f051df67ca56089b30946ef1079c98d
Frederik Agerkvist <s153565@student.dtu.dk>
2018-10-08 22:56:08 +0200

    test image

--- app/src/main/java/com/example/matos/project1/Product.java
+++ app/src/main/java/com/example/matos/project1/Product.java
@@ -2,12 +2,32 @@ package com.example.matos.project1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class Product extends AppCompatActivity {
+
+
+    RatingBar ratingBar;
+    TextView score;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        final RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        final TextView score = (TextView) findViewById(R.id.score);
+
+        DecimalFormat df1 = new DecimalFormat(".#");
+        score.setText("" + df1.format(4.0));
+
+        ratingBar.setRating(Float.parseFloat(df1.format(4.0)));
+
     }
+
+
 }

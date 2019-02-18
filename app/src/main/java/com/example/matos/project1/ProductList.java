5a2269977747033833594da610cdb633a71f4f66
schmi <schmidt13@live.dk>
2019-02-18 15:46:18 +0100

    API

--- app/src/main/java/com/example/matos/project1/ProductList.java
+++ app/src/main/java/com/example/matos/project1/ProductList.java
@@ -4,16 +4,25 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class ProductList extends AppCompatActivity {
 
+    ImageView testBtn;
     ListView listView;
     ArrayList<JSONObject> jsons = new ArrayList<>();
 
@@ -26,6 +35,18 @@ public class ProductList extends AppCompatActivity {
         listView = findViewById(R.id.listView);
 
         new setupList().execute();
+
+
+        //TestAPI
+        testBtn = findViewById(R.id.testBtn);
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new callAPI().execute();
+
+            }
+        });
 
     }
 
@@ -80,6 +101,51 @@ public class ProductList extends AppCompatActivity {
             jsons.add(json);
         }
         return jsons;
+    }
+
+    private class callAPI extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected void onPreExecute(){
+
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+
+                URL url = new URL("http://easyeats.dk/test.php");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+
+                bufferedReader.close();
+                urlConnection.disconnect();
+
+                return stringBuilder.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String response){
+
+            System.out.println("Response from API is: " + response);
+
+        }
+
     }
 
 }

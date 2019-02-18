20b4621ec688246fc126e6cd5dc0c7c55c623bd7
Frederik Agerkvist <s153565@student.dtu.dk>
2019-02-18 16:16:12 +0100

    Login_activity added

--- app/src/main/java/com/example/matos/project1/LoginActivity.java
+++ app/src/main/java/com/example/matos/project1/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.matos.project1;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -324,7 +325,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
 
             // TODO: register the new account here.
-            return true;
+            return false;
         }
 
         @Override
@@ -334,6 +335,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 finish();
+                Intent myIntent = new Intent(LoginActivity.this,HomeActivity.class);
+                LoginActivity.this.startActivity(myIntent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();

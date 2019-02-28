59ed19c1024d8b06d6b5c98869bf325d70df4e06
Frederik Agerkvist <s153565@student.dtu.dk>
2019-02-28 16:45:02 +0100

    Big design remake, Profile is begun, but minor fault, you can't click on fivorites etc.

--- app/src/main/java/com/example/matos/project1/HomeActivity.java
+++ app/src/main/java/com/example/matos/project1/HomeActivity.java
@@ -1,36 +1,57 @@
 package com.example.matos.project1;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button scan, browse, favorites, recents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        scan = findViewById(R.id.scan);
-        browse = findViewById(R.id.browse);
-        favorites = findViewById(R.id.favorites);
-        recents = findViewById(R.id.recent);
-
-        favorites.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                favorites();
-            }
-        });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navgationListner);
 
     }
 
-    public void favorites() {
-        Intent intent = new Intent(this, ProductList.class);
-        startActivity(intent);
-    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navgationListner =
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+
+                        switch (item.getItemId()) {
+                            case R.id.nav_home:
+                                    selectedFragment = new FragmentHome();
+                            break;
+                            case R.id.nav_search:
+                                selectedFragment = new FragmentHome();
+                                break;
+                            case R.id.nav_profile:
+                                selectedFragment = new FragmentProfile();
+                                break;
+                            case R.id.nav_camera:
+                                selectedFragment = new FragmentHome();
+                                break;
+                        }
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                        return true;
+                    }
+                };
+
+
+
+
+
 }

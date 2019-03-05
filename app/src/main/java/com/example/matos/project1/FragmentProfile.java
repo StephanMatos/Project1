ba0d26bdf0baea4e65b4090feb919adfd438eb4a
Frederik Agerkvist <s153565@student.dtu.dk>
2019-03-05 14:10:50 +0100

    it's beginning to look a lot like christmas...

--- app/src/main/java/com/example/matos/project1/FragmentProfile.java
+++ app/src/main/java/com/example/matos/project1/FragmentProfile.java
@@ -1,5 +1,7 @@
 package com.example.matos.project1;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,16 +11,20 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class FragmentProfile extends Fragment{
 
+    Context context;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        context = container.getContext();
 
         return view;
     }
@@ -27,6 +33,9 @@ public class FragmentProfile extends Fragment{
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
         LinearLayout favoriteLayout = view.findViewById(R.id.favoriteLayout);
+        LinearLayout recentLayout = view.findViewById(R.id.recentLayout);
+        ImageView changepicture = view.findViewById(R.id.changePicture);
+
         favoriteLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +44,34 @@ public class FragmentProfile extends Fragment{
                 startActivity(intent);
             }
         });
+
+        recentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ProductList.class);
+                intent.putExtra("type", "recents");
+                startActivity(intent);
+            }
+        });
+
+        changepicture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // create a Dialog component
+                final Dialog dialog = new Dialog(context);
+
+                dialog.setContentView(R.layout.dialogview_profile_pictures);
+                dialog.setTitle("Change profile picture");
+
+
+                dialog.show();
+            }
+        });
+
+
+
     }
 
 

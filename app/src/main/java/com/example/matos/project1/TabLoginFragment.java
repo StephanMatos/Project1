bcfae5a80aa00d980ff31621e13e4c40d300aeee
Matos <stephanmatos@gmail.com>
2019-04-16 14:35:07 +0200

    Login Async Fragment

--- app/src/main/java/com/example/matos/project1/TabLoginFragment.java
+++ app/src/main/java/com/example/matos/project1/TabLoginFragment.java
@@ -1,16 +1,20 @@
 package com.example.matos.project1;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TabLoginFragment extends Fragment {
-
-
+    EditText email, password;
+    Button login;
+    ProgressDialog progressDialog;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -19,4 +23,32 @@ public class TabLoginFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        email = view.findViewById(R.id.email_TextView);
+        password = view.findViewById(R.id.pass_TextView);
+
+        login = view.findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attempt_login();
+            }
+
+
+        });
+
+
+    }
+    private void attempt_login() {
+
+        new AsyncLogin(getContext()).execute(email.getText().toString(),password.getText().toString());
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+    }
+
+
 }

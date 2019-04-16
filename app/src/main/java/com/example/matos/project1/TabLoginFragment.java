0cb4595d95a9417fabdea9f6113300d38fee293a
Frederik Agerkvist <s153565@student.dtu.dk>
2019-04-16 11:10:10 +0200

    New Login layout done, need functionality

---
+++ app/src/main/java/com/example/matos/project1/TabLoginFragment.java
@@ -0,0 +1,22 @@
+package com.example.matos.project1;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabLoginFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_login,container,false);
+
+        return view;
+    }
+}

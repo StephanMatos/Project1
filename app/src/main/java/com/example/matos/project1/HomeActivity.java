a0e89f28899c6ad7295552a10ce6dea59b30f190
Frederik Agerkvist <s153565@student.dtu.dk>
2019-04-29 13:06:31 +0200

    Open Home-fragment when user has logged in

--- app/src/main/java/com/example/matos/project1/HomeActivity.java
+++ app/src/main/java/com/example/matos/project1/HomeActivity.java
@@ -1,14 +1,12 @@
 package com.example.matos.project1;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,9 +18,9 @@ public class HomeActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(navgationListner);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHome()).commit();
 
     }
-
     private BottomNavigationView.OnNavigationItemSelectedListener navgationListner =
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override

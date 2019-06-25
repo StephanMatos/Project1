1d382df0a00fb6809addf40f88720b97989b7664
Matos <stephanmatos@gmail.com>
2019-06-25 16:44:50 +0200

    Json post added

--- app/src/main/java/com/example/matos/project1/Users/AsyncLogin.java
+++ app/src/main/java/com/example/matos/project1/Users/AsyncLogin.java
@@ -8,7 +8,11 @@ import com.example.matos.project1.Menu.HomeActivity;
 import com.example.matos.project1.SavedValues;
 import com.example.matos.project1.SplashScreenActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -36,13 +40,29 @@ public class AsyncLogin extends AsyncTask<String,Void,Void> {
         password = Strings[1];
 
         try {
-            String LoginUrl = "https://easyeats.dk/EasyEats/login.php?email="+email+"&password="+password;
+            String LoginUrl = "https://easyeats.dk/EasyEats/login.php?";
             System.out.println(LoginUrl);
 
             URL url = new URL(LoginUrl);
 
 
+
             HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+            connection.setRequestProperty("Accept","application/json");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            JSONObject jsonLogin = new JSONObject();
+            jsonLogin.put("email",email);
+            jsonLogin.put("password",password);
+            DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(jsonLogin.toString());
+            outputStream.flush();
+            outputStream.close();
+
+
             InputStream inputStream = connection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             data = bufferedReader.readLine();
@@ -73,7 +93,7 @@ public class AsyncLogin extends AsyncTask<String,Void,Void> {
             }
 
 
-        }catch (IOException | NullPointerException e){
+        }catch (IOException | NullPointerException | JSONException e){
             TabLoginFragment.unknown = true;
 
             e.printStackTrace();

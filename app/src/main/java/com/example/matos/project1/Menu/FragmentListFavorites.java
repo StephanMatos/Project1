ce4384e0c0f848eb2d6f1f5c5dfcfb9d23d521e8
schmi <schmidt13@live.dk>
2019-06-26 15:20:38 +0200

    Dansk

--- app/src/main/java/com/example/matos/project1/Menu/FragmentListFavorites.java
+++ app/src/main/java/com/example/matos/project1/Menu/FragmentListFavorites.java
@@ -40,7 +40,7 @@ public class FragmentListFavorites extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         TextView tv = view.findViewById(R.id.titleTextView);
-        tv.setText("Favorites");
+        tv.setText(R.string.profile_fact_2);
         listView = view.findViewById(R.id.listView);
         cameraBtn = getActivity().findViewById(R.id.FAB);
         ItemAdapter itemAdapter = new ItemAdapter(getActivity(), HomeActivity.favorites);
